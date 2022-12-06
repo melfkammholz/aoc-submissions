4e7ee752850bfa3d4aeccbedb3ec950db6fac682
fwcd <fwcdmail@gmail.com>
2022-12-06 22:07:08 +0100

    Add solutionWebUrl
    ...to be used later

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 /** Generates solution urls for GitHub. */
 function gitHubUrls({ user, repo, branch = "main", path }) {
   return {
-    solutionUrl: (day, part) => `https://raw.githubusercontent.com/${user}/${repo}/${branch}/${path(day, part)}`
+    solutionUrl: (day, part) => `https://raw.githubusercontent.com/${user}/${repo}/${branch}/${path(day, part)}`,
+    solutionWebUrl: (day, part) => `https://github.com/${user}/${repo}/blob/${branch}/${path(day, part)}`,
   };
 }
 
