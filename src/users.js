e6675cbc18d79ec65dc9f2b62909f9c124dda1e5
Melf Kammholz <melf.kammholz@gmail.com>
2023-11-08 22:09:40 +0100

    prepare for 2023 :)

--- src/users.js
+++ src/users.js
@@ -3,8 +3,9 @@ import { gitHubUrls, pad } from './utils.js';
 
 /** Loads the list of users. */
 export async function loadUsers() {
-  const fwcdPaths = await (await fetch(`https://raw.githubusercontent.com/fwcd/advent-of-code-${year}/main/paths.json`)).json();
-  const estgPaths = await (await fetch(`https://raw.githubusercontent.com/estugon/advent-of-code-${year}/main/paths.json`)).json();
+  // TODO handle error if necessary?
+  const fwcdPaths = await (await fetch(`https://raw.githubusercontent.com/fwcd/advent-of-code-${year}/main/paths.json`)).json().catch(() => ({}));
+  const estgPaths = await (await fetch(`https://raw.githubusercontent.com/estugon/advent-of-code-${year}/main/paths.json`)).json().catch(() => ({}));
 
   const users = [
     {
