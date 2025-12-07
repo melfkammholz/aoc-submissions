b99d3d43cb913bea3d80446e9c898b2f943678cb
Melf Kammholz <melf.kammholz@gmail.com>
2025-12-07 13:12:59 +0100

    comment out inactive users

--- src/users.js
+++ src/users.js
@@ -4,15 +4,15 @@ import { gitHubUrls, pad } from './utils.js';
 /** Loads the list of users. */
 export async function loadUsers() {
   // TODO handle error if necessary?
-  const fwcdPaths = await (await fetch(`https://raw.githubusercontent.com/fwcd/advent-of-code-${year}/main/paths.json`)).json().catch(() => ({}));
+  // const fwcdPaths = await (await fetch(`https://raw.githubusercontent.com/fwcd/advent-of-code-${year}/main/paths.json`)).json().catch(() => ({}));
   const SGPaths = await (await fetch(`https://raw.githubusercontent.com/SergejGleithmann/aoc/main/${year}/paths.json`)).json().catch(() => ({}));
-  const kazumiPaths = await (await fetch(`https://raw.githubusercontent.com/Dormanil/Advent-of-Code/${year}/exceptionInfo.json`)).json().catch(() => ({}));
-  const magi3rPaths = await (await fetch(`https://raw.githubusercontent.com/Magi3r/AoC-${year}/main/paths.json`)).json().catch(() => ({}));
-  const hendrick404Paths = await (await fetch(`https://raw.githubusercontent.com/hendrick404/advent-of-code-${year}/main/paths.json`)).json().catch(() => ({}));
+  // const kazumiPaths = await (await fetch(`https://raw.githubusercontent.com/Dormanil/Advent-of-Code/${year}/exceptionInfo.json`)).json().catch(() => ({}));
+  // const magi3rPaths = await (await fetch(`https://raw.githubusercontent.com/Magi3r/AoC-${year}/main/paths.json`)).json().catch(() => ({}));
+  // const hendrick404Paths = await (await fetch(`https://raw.githubusercontent.com/hendrick404/advent-of-code-${year}/main/paths.json`)).json().catch(() => ({}));
 
-  const fwcdSolution = (day, part) => (fwcdPaths[day]?.parts ?? [])[part] ?? fwcdPaths[day];
+  // const fwcdSolution = (day, part) => (fwcdPaths[day]?.parts ?? [])[part] ?? fwcdPaths[day];
   const SGSolution = (day, part) => (SGPaths[day]?.parts ?? [])[part] ?? SGPaths[day];
-  const magi3rSolution = (day, part) => (magi3rPaths[day]?.parts ?? [])[part] ?? magi3rPaths[day];
+  // const magi3rSolution = (day, part) => (magi3rPaths[day]?.parts ?? [])[part] ?? magi3rPaths[day];
 
   const users = [
     {
@@ -26,17 +26,17 @@ export async function loadUsers() {
         path: (day, part) => `AdventOfCode/${year}/${day + 1}/${part + 1}.cc`
       })
     },
-    {
-      name: "I3J03RN",
-      lang: _ => "clike",
-      langName: _ => "C++",
-      ...gitHubUrls({
-        user: "I3J03RN",
-        repo: "ProgrammingChallenges",
-        branch: "master",
-        path: day => `AoC/${year}/${day + 1}.cc`
-      })
-    },
+    // {
+    //   name: "I3J03RN",
+    //   lang: _ => "clike",
+    //   langName: _ => "C++",
+    //   ...gitHubUrls({
+    //     user: "I3J03RN",
+    //     repo: "ProgrammingChallenges",
+    //     branch: "master",
+    //     path: day => `AoC/${year}/${day + 1}.cc`
+    //   })
+    // },
     {
       name: "Melf",
       lang: _ => "haskell",
@@ -47,28 +47,28 @@ export async function loadUsers() {
         path: (day, part) => `Day${pad(day + 1, 2)}/${["A", "B"][part]}.hs`
       })
     },
-    {
-      name: "fwcd",
-      lang: (day, part) => fwcdSolution(day, part)?.lang?.codemirror,
-      langAnnotation: "Today: ",
-      langName: (day, part) => fwcdSolution(day, part)?.lang?.name ?? "Unknown",
-      encoding: (day, part) => fwcdSolution(day, part)?.encoding,
-      ...gitHubUrls({
-        user: "fwcd",
-        repo: `advent-of-code-${year}`,
-        path: (day, part) => fwcdSolution(day, part)?.path
-      })
-    },
-    {
-      name: "Yorik Hansen",
-      lang: _ => "python",
-      langName: _ => "Python",
-      ...gitHubUrls({
-        user: "YorikHansen",
-        repo: "AdventOfCode",
-        path: (day, part) => `${year}/day${pad(day + 1, 2)}/part${part + 1}.py`
-      })
-    },
+    // {
+    //   name: "fwcd",
+    //   lang: (day, part) => fwcdSolution(day, part)?.lang?.codemirror,
+    //   langAnnotation: "Today: ",
+    //   langName: (day, part) => fwcdSolution(day, part)?.lang?.name ?? "Unknown",
+    //   encoding: (day, part) => fwcdSolution(day, part)?.encoding,
+    //   ...gitHubUrls({
+    //     user: "fwcd",
+    //     repo: `advent-of-code-${year}`,
+    //     path: (day, part) => fwcdSolution(day, part)?.path
+    //   })
+    // },
+    // {
+    //   name: "Yorik Hansen",
+    //   lang: _ => "python",
+    //   langName: _ => "Python",
+    //   ...gitHubUrls({
+    //     user: "YorikHansen",
+    //     repo: "AdventOfCode",
+    //     path: (day, part) => `${year}/day${pad(day + 1, 2)}/part${part + 1}.py`
+    //   })
+    // },
     {
       name: "Skgland",
       lang: _ => "rust",
@@ -79,17 +79,17 @@ export async function loadUsers() {
         path: day => `crates/year${year}/src/day${pad(day + 1, 2)}.rs`
       })
     },
-    {
-      name: "b3z",
-      lang: _ => "python",
-      langName: _ => "Python",
-      ...gitHubUrls({
-        user: "b3z",
-        repo: "aoc",
-        branch: "master",
-        path: (day, part) => `${year}/${pad(day + 1, 2)}/${part + 1}.py`
-      })
-    },
+    // {
+    //   name: "b3z",
+    //   lang: _ => "python",
+    //   langName: _ => "Python",
+    //   ...gitHubUrls({
+    //     user: "b3z",
+    //     repo: "aoc",
+    //     branch: "master",
+    //     path: (day, part) => `${year}/${pad(day + 1, 2)}/${part + 1}.py`
+    //   })
+    // },
     {
       name: "H1tchhiker",
       lang: _ => "typescript",
@@ -120,51 +120,51 @@ export async function loadUsers() {
         path: day => `src/Day${day + 1}.hs`
       })
     },
-    {
-      name: "maclement",
-      lang: _ => "haskell",
-      langName: _ => "Haskell",
-      ...gitHubUrls({
-        user: "maclement",
-        repo: `advent-of-code-${year}`,
-        path: day => `Haskell/Day${day + 1}/A.hs`
-      })
-    },
-    {
-      name: "Magi3r",
-      lang: (day, part) => magi3rSolution(day, part)?.lang?.codemirror,
-      langAnnotation: "Today: ",
-      langName: (day, part) => magi3rSolution(day, part)?.lang?.name ?? "Unknown",
-      encoding: (day, part) => magi3rSolution(day, part)?.encoding,
-      ...gitHubUrls({
-        user: "Magi3r",
-        repo: `AoC-${year}`,
-        path: (day, part) => magi3rSolution(day, part)?.path
-      })
-    },
-    {
-      name: "sebfisch",
-      lang: _ => "zig",
-      langName: _ => "Zig",
-      ...gitHubUrls({
-        user: "sebfisch",
-        repo: "AdventOfCode",
-        branch: "latest",
-        path: (day, part) => `year${year}/day${pad(day + 1, 2)}/task${part + 1}.zig`
-      })
-    },
-    {
-      name: "Kazumi",
-      lang: day => kazumiPaths[`${day + 1}`]?.lang ?? "csharp",
-      langAnnotation: "Today: ",
-      langName: day => kazumiPaths[`${day + 1}`]?.langName ?? "C#",
-      ...gitHubUrls({
-        user: "Dormanil",
-        repo: "Advent-of-Code",
-        branch: `${year}`,
-        path: day => `Dec${day + 1}/Program.${kazumiPaths[`${day + 1}`]?.extension ?? "cs"}`
-      })
-    },
+    // {
+    //   name: "maclement",
+    //   lang: _ => "haskell",
+    //   langName: _ => "Haskell",
+    //   ...gitHubUrls({
+    //     user: "maclement",
+    //     repo: `advent-of-code-${year}`,
+    //     path: day => `Haskell/Day${day + 1}/A.hs`
+    //   })
+    // },
+    // {
+    //   name: "Magi3r",
+    //   lang: (day, part) => magi3rSolution(day, part)?.lang?.codemirror,
+    //   langAnnotation: "Today: ",
+    //   langName: (day, part) => magi3rSolution(day, part)?.lang?.name ?? "Unknown",
+    //   encoding: (day, part) => magi3rSolution(day, part)?.encoding,
+    //   ...gitHubUrls({
+    //     user: "Magi3r",
+    //     repo: `AoC-${year}`,
+    //     path: (day, part) => magi3rSolution(day, part)?.path
+    //   })
+    // },
+    // {
+    //   name: "sebfisch",
+    //   lang: _ => "zig",
+    //   langName: _ => "Zig",
+    //   ...gitHubUrls({
+    //     user: "sebfisch",
+    //     repo: "AdventOfCode",
+    //     branch: "latest",
+    //     path: (day, part) => `year${year}/day${pad(day + 1, 2)}/task${part + 1}.zig`
+    //   })
+    // },
+    // {
+    //   name: "Kazumi",
+    //   lang: day => kazumiPaths[`${day + 1}`]?.lang ?? "csharp",
+    //   langAnnotation: "Today: ",
+    //   langName: day => kazumiPaths[`${day + 1}`]?.langName ?? "C#",
+    //   ...gitHubUrls({
+    //     user: "Dormanil",
+    //     repo: "Advent-of-Code",
+    //     branch: `${year}`,
+    //     path: day => `Dec${day + 1}/Program.${kazumiPaths[`${day + 1}`]?.extension ?? "cs"}`
+    //   })
+    // },
     {
       name: "Sesquil",
       lang: _ => "python",
@@ -185,36 +185,36 @@ export async function loadUsers() {
         path: (day, part) => `${year}/day${pad(day + 1, 2)}/puzzle${part + 1}.ua`
       })
     },
-    {
-      name: "xtay2",
-      lang: _ => "java",
-      langName: _ => "Java",
-      ...gitHubUrls({
-        user: "xtay2",
-        repo: "AdventOfCode",
-        path: (day, part) => `src/year${year}/day${pad(day + 1, 2)}/Task_${["A", "B"][part]}.java`
-      })
-    },
-    {
-      name: "JaGitJe",
-      lang: _ => "haskell",
-      langName: _ => "Haskell",
-      ...gitHubUrls({
-        user: "JaGitJe",
-        repo: `adventofcode${shortYear}`,
-        path: (day, part) => `src/Day${pad(day + 1, 2)}.hs`
-      })
-    },
-    {
-      name: "CoolCode2020",
-      lang: _ => "python",
-      langName: _ => "Python",
-      ...gitHubUrls({
-        user: "CoolCode2020",
-        repo: "AOC",
-        path: (day, part) => `/Tag${day + 1}/day${day + 1}.py`
-      })
-    },
+    // {
+    //   name: "xtay2",
+    //   lang: _ => "java",
+    //   langName: _ => "Java",
+    //   ...gitHubUrls({
+    //     user: "xtay2",
+    //     repo: "AdventOfCode",
+    //     path: (day, part) => `src/year${year}/day${pad(day + 1, 2)}/Task_${["A", "B"][part]}.java`
+    //   })
+    // },
+    // {
+    //   name: "JaGitJe",
+    //   lang: _ => "haskell",
+    //   langName: _ => "Haskell",
+    //   ...gitHubUrls({
+    //     user: "JaGitJe",
+    //     repo: `adventofcode${shortYear}`,
+    //     path: (day, part) => `src/Day${pad(day + 1, 2)}.hs`
+    //   })
+    // },
+    // {
+    //   name: "CoolCode2020",
+    //   lang: _ => "python",
+    //   langName: _ => "Python",
+    //   ...gitHubUrls({
+    //     user: "CoolCode2020",
+    //     repo: "AOC",
+    //     path: (day, part) => `/Tag${day + 1}/day${day + 1}.py`
+    //   })
+    // },
     {
       name: "hype09",
       lang: _ => "fsharp",
@@ -246,28 +246,28 @@ export async function loadUsers() {
         path: (day, part) => `${pad(day + 1, 2)}.py`
       })
     },
-    {
-      name: "Hendrik",
-      lang: day => hendrick404Paths[day]?.lang.identifier,
-      langName: day => hendrick404Paths[day]?.lang.name ?? "Unknown",
-      langAnnotation: "Today: ",
-      ...gitHubUrls({
-        user: "hendrick404",
-        repo: `advent-of-code-${year}`,
-        branch: "main",
-        path: (day, part) => hendrick404Paths[day]?.path ?? ((hendrick404Paths[day]?.paths ?? [])[part])
-      })
-    },
-    {
-      name: "Roman",
-      lang: _ => "python",
-      langName: _ => "Python",
-      ...gitHubUrls({
-        user: "romanhemens",
-        repo: `advent_of_code2024`,
-        path: (day, part) => `Day${day + 1}/${part === 0 ? "first" : "second"}.py`
-      })
-    },
+    // {
+    //   name: "Hendrik",
+    //   lang: day => hendrick404Paths[day]?.lang.identifier,
+    //   langName: day => hendrick404Paths[day]?.lang.name ?? "Unknown",
+    //   langAnnotation: "Today: ",
+    //   ...gitHubUrls({
+    //     user: "hendrick404",
+    //     repo: `advent-of-code-${year}`,
+    //     branch: "main",
+    //     path: (day, part) => hendrick404Paths[day]?.path ?? ((hendrick404Paths[day]?.paths ?? [])[part])
+    //   })
+    // },
+    // {
+    //   name: "Roman",
+    //   lang: _ => "python",
+    //   langName: _ => "Python",
+    //   ...gitHubUrls({
+    //     user: "romanhemens",
+    //     repo: `advent_of_code2024`,
+    //     path: (day, part) => `Day${day + 1}/${part === 0 ? "first" : "second"}.py`
+    //   })
+    // },
     {
       name: "Sergej Gleithmann",
       lang: (day, part) => SGSolution(day, part)?.lang?.codemirror,
@@ -290,16 +290,16 @@ export async function loadUsers() {
         path: (day, part) => `${year}/elixir/solutions/day${pad(day + 1, 2)}.ex`
       })
     },
-    {
-      name: "Bennit",
-      lang: _ => "python",
-      langName: _ => "Python",
-      ...gitHubUrls({
-        user: "Bennit99",
-        repo: `AdventOfCode`,
-        path: (day, part) => `${year}/${day + 1}${["a", "b"][part]}.py`
-      })
-    },
+    // {
+    //   name: "Bennit",
+    //   lang: _ => "python",
+    //   langName: _ => "Python",
+    //   ...gitHubUrls({
+    //     user: "Bennit99",
+    //     repo: `AdventOfCode`,
+    //     path: (day, part) => `${year}/${day + 1}${["a", "b"][part]}.py`
+    //   })
+    // },
     {
       name: "ggb",
       lang: _ => "clojure",
